2deb988f22d0b64ddb1648c40caef39d52a1a062
Sebastian <Sebastian@DESKTOP-FC2DPB2>
2024-10-25 13:14:04 +0200

    Add project files.

---
+++ ListBox_Inheritance/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListBox_Inheritance
+{
+    abstract public class Animal 
+    {
+        private String name;
+        public String Name
+        { get { return name; } }
+        public Animal(String name)
+        {
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return $"{name} ({Sound})";
+        }
+        abstract public String Sound
+        {
+            get;
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(String name) : base(name)
+        {
+        }
+        public override String Sound
+        { 
+            get { return "bark"; }
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(String name) : base(name)
+        {
+        }
+        public override String Sound
+        {
+            get { return "meow"; }
+        }
+    }
+
+
+}
---
+++ ListBox_Inheritance/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBox_Inheritance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBoxObjects.Items.Add(1);
+            listBoxObjects.Items.Add("Name");
+            listBoxObjects.Items.Add(2.3);
+            listBoxObjects.Items.Add(DateTime.Now);
+            //listBoxObjects.Items.Add(new Animal("Sth"));
+            listBoxObjects.Items.Add(new Dog("Reksio"));
+            listBoxObjects.Items.Add(new Cat("Bonifacy"));
+        }
+
+        private void listBoxObjects_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBoxObjects.SelectedIndex != -1)
+            {
+                Object myObject = listBoxObjects.SelectedItem;
+
+                MessageBox.Show( myObject.GetType().ToString());
+
+                //first way
+                Animal animal = myObject as Animal;
+                if (animal != null)
+                {
+                }
+                //second way
+                if(myObject is Animal)
+                {
+                    Animal a = myObject as Animal;
+                }
+
+
+            }
+
+
+        }
+    }
+}
